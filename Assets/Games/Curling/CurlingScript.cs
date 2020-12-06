e2cfcf379268c80edf53defbae1046925e04fe7e
Mary Abbott <mabbott9@gatech.edu>
2020-12-06 00:22:12 -0500

    curling

---
+++ Assets/Games/Curling/CurlingScript.cs
@@ -0,0 +1,19 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class CurlingScript : UdonSharpBehaviour
+{
+    private Vector3 InitialPosition;
+    void Start()
+    {
+        InitialPosition = this.transform.position;
+    }
+
+    public void RespawnCurlingThing()
+    {
+        this.transform.position = InitialPosition;
+    }
+}
